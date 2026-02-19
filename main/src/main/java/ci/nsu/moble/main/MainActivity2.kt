f78b238a2c1f9b389896383fc3b9bae3ac3fc2ff
GrEmzEz <shaposhnikovaa@mer.ci.nsu.ru>
2026-02-19 16:12:22 +0700

    /

---
+++ main/src/main/java/ci/nsu/moble/main/MainActivity2.kt
@@ -0,0 +1,45 @@
+package ci.nsu.moble.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ci.nsu.moble.main.ui.theme.PracticeTheme
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PracticeTheme {
+                    Text(text = "Roblox Studio Manager", modifier = Modifier.padding(15.dp))
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PracticeTheme {
+        Greeting("Android")
+    }
+}
---
+++ main/src/test/java/ci/nsu/moble/main/Activity.kt
@@ -0,0 +1,11 @@
+package ci.nsu.moble.main
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class Activity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
